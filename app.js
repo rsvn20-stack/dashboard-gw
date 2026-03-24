0fa70beca3bb1e6c703e1dbfc8c079430194f916
rsvn20-stack <rsvn20@gmail.com>
2026-03-24 12:22:50 +0700

    Add files via upload
    first upload

---
+++ app.js
@@ -0,0 +1,71 @@
+// IMPORT FIREBASE
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// CONFIG (GANTI PUNYA LU)
+const firebaseConfig = {
+  apiKey: "AIzaSyAYml7-V58EnkcHLZs_nM1jik67sla_jbE",
+  authDomain: "dashboard-f24f3.firebaseapp.com",
+  projectId: "dashboard-f24f3",
+  storageBucket: "dashboard-f24f3.firebasestorage.app",
+  messagingSenderId: "944792346729",
+  appId: "1:944792346729:web:1eddfc128c1bb93cd842f8",
+  measurementId: "G-XHGFNJ5Z3P"
+};
+
+// INIT
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// REGISTER
+window.register = function () {
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+
+  createUserWithEmailAndPassword(auth, email, pass)
+    .then(() => alert("Register berhasil"))
+    .catch(e => alert(e.message));
+};
+
+// LOGIN
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, pass)
+    .then(() => {
+      window.location.href = "index.html";
+    })
+    .catch(e => alert(e.message));
+};
+
+// TAMBAH DOMAIN
+window.addDomain = async function () {
+  const input = document.getElementById("domainInput");
+
+  await addDoc(collection(db, "domains"), {
+    name: input.value
+  });
+
+  input.value = "";
+  loadDomains();
+};
+
+// LOAD DOMAIN
+async function loadDomains() {
+  const list = document.getElementById("list");
+  if (!list) return;
+
+  list.innerHTML = "";
+
+  const querySnapshot = await getDocs(collection(db, "domains"));
+
+  querySnapshot.forEach(doc => {
+    list.innerHTML += `<li>${doc.data().name}</li>`;
+  });
+}
+
+// AUTO LOAD
+loadDomains();
